3acaf68d953db72770ab5df15c9d674080d0738a
Austin Chinn <achinn@ucsd.edu>
2016-01-21 02:35:59 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,39 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(submitClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	jumbotronHeader.text(projectTitle);
+	//$(this).css("background-color","#7fff00");
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+}
+
+function submitClick(e) {
+	var projectName = $("#project").val();
+	$(projectName).animate({
+		width: $("#width").val()
+	});
+
+	var newText = $('#description').val();
+	$(projectName + " .project-description").text(newText);
 }
